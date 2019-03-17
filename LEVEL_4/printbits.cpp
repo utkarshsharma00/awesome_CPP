4e5bc92f971359d426a153d8dc4d13a3600c4065
Utkarsh Sharma <flamingice2801@gmail.com>
2019-03-17 23:57:25 +0530

    Update printbits.cpp

--- LEVEL_4/printbits.cpp
+++ LEVEL_4/printbits.cpp
@@ -1,12 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(int argc, char**argv){
+int main(int argc, char **argv)
+{
     int x;
-    cin>>x;
+    cin >> x;
 
-    for(int bn=31;bn>=0;bn--){
-        int bm = 1<<bm;
-        cout<<((x&bm)==0?0:10);
-    }cout<<endl;
+    for (int bn = 31; bn >= 0; bn--) //bn means bitnumber
+    {
+        int bm = 1 << bn;
+        cout << ((x & bm) == 0 ? 0 : 1);
+    }
+    cout << endl;
 }
