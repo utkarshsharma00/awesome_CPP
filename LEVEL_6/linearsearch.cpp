add137af1f403a2c63012c015c7f6d4339cbb0ec
Utkarsh Sharma <flamingice2801@gmail.com>
2019-03-20 20:33:17 +0530

    Add files via upload

---
+++ LEVEL_6/linearsearch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int arr[] = {50, 60, 70, 90, 2, 33, -14, 61};
+    int find;
+    cin >> find;
+
+    for (int i = 0; i < 8; i++)
+    {
+        if (find == arr[i])
+        {
+            cout << i;
+            return 0;
+        }
+    }
+    cout << -1;
+}
