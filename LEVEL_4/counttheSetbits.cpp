6721713efec6b6601d8a8ab5f920e74011c2c6d9
Utkarsh Sharma <flamingice2801@gmail.com>
2019-03-23 20:44:33 +0530

    Update counttheSetbits.cpp

--- LEVEL_4/counttheSetbits.cpp
+++ LEVEL_4/counttheSetbits.cpp
@@ -11,6 +11,8 @@ int main(int argc, char **argv)
     {
         int rsb = (num & (-num)); // or can be done as (num & ((~num)+1)) which is basically 2's complement
         num = num - rsb;
+        // int rsb = num - 1; //* these 14 and 15 th lines are alternative to the 12 and 13th line. 14th and 15th are more elegant
+        // num = num & (rsb);
         count++;
     }
     cout << count;
