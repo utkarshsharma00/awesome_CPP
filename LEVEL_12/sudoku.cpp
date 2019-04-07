a7044c78393b64417dda2b5d9dd9b37a1eefb734
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-07 23:08:48 +0530

    Update sudoku.cpp

--- LEVEL_12/sudoku.cpp
+++ LEVEL_12/sudoku.cpp
@@ -3,6 +3,41 @@
 #include <string>
 using namespace std;
 
+void sudoku(vector<vector<int>> &board, int box_no)
+{
+    if (box_no == board.size() * board.size()) // see carefully for assignment and comparison operators and logic
+    {
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board.size(); j++)
+            {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+        return;
+    }
+
+    int i = box_no / board.size(); // i = rows
+    int j = box_no % board.size(); // j = cols
+    if (board[i][j] == 0)
+    {
+        for (int possible_options = 1; possible_options <= 9; possible_options++)
+        {
+            if (is_the_number_safe(board, i, j, possible_options) == true)
+            {
+                board[i][j] = possible_options;
+                sudoku(board, box_no + 1);
+                board[i][j] = 0;
+            }
+        }
+    }
+    else
+    {
+        sudoku(board, box_no + 1); //place is filled with the correct option, directly jump to the next
+    }
+}
+
 int main(int argc, char **argv)
 {
     vector<vector<int>> board{
