5e0d3f371563b0c356b8757b92058b9888ff403d
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-07 23:08:11 +0530

    Create sudoku.cpp

---
+++ LEVEL_12/sudoku.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    vector<vector<int>> board{
+        {3, 0, 6, 5, 0, 8, 4, 0, 0},
+        {5, 2, 0, 0, 0, 0, 0, 0, 0},
+        {0, 8, 7, 0, 0, 0, 0, 3, 1},
+        {0, 0, 3, 0, 1, 0, 0, 8, 0},
+        {9, 0, 0, 8, 6, 3, 0, 0, 5},
+        {0, 5, 0, 0, 9, 0, 6, 0, 0},
+        {1, 3, 0, 0, 0, 0, 2, 5, 0},
+        {0, 0, 0, 0, 0, 0, 0, 7, 4},
+        {0, 0, 5, 2, 0, 6, 3, 0, 0},
+    };
+    sudoku(board, 0);
+}
