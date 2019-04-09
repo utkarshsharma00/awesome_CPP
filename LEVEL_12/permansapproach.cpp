534293abbb6b87c1c10ad9134da32aa987a36af6
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-09 23:11:49 +0530

    Add files via upload

---
+++ LEVEL_12/permansapproach.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void perma(string ques, string ans)
+{
+    if (ques.size() == 0)
+    {
+        cout << ans << endl;
+        return;
+    }
+
+    char ch = ques[0];
+    string ros = ques.substr(1);
+    for (int i = 0; i < ans.length() + 1; i++)
+    {
+        string left = ans.substr(0, i);
+        string right = ans.substr(i);
+        perma(ros, left + ch + right);
+    }
+}
+int main(int argc, char **argv)
+{
+    perma("abc", "");
+}
