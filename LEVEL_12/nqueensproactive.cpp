70acb0c0e4859148d86d9351a918a855420a54b7
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-11 20:55:58 +0530

    Create nqueensproactive.cpp

---
+++ LEVEL_12/nqueensproactive.cpp
@@ -0,0 +1,18 @@
+#include <iostream> //* Proactive
+#include <vector>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    vector<vector<bool>> board = {
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+
+    };
+    nqueens(board, 0, -1, "");
+    cout << count;
+}
