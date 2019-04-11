7e5c5fe2c31853b3de02eb5a66d6be6b4c62b7ae
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-11 20:53:49 +0530

    Update nqueensreactive.cpp

--- LEVEL_12/nqueensreactive.cpp
+++ LEVEL_12/nqueensreactive.cpp
@@ -2,6 +2,28 @@
 #include <vector>
 using namespace std;
 
+int count = 0;
+bool is_the_queen_safe(vector<vector<bool>> &board, int i, int j)
+{
+    //horizontal
+    for (int jj = 0; jj < board.size(); jj++)
+    {
+        if (board[i][jj] == true && jj != j) // jj!=j is checked to determine that the next queen doesnt check for the position at which already a queen is placed
+        {
+            return false;
+        }
+    }
+    //vertical
+    for (int ii = 0; ii < board.size(); ii++)
+    {
+        if (board[ii][j] == true && ii != i)
+        {
+            return false;
+        }
+    }
+    
+}    
+
 bool is_the_box_safe(vector<vector<bool>> &board)
 {
     for (int i = 0; i < board.size(); i++)
