a0e41fc9e6eee27486858460b0586ce74404f59a
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-11 20:58:15 +0530

    Update nqueensproactive.cpp

--- LEVEL_12/nqueensproactive.cpp
+++ LEVEL_12/nqueensproactive.cpp
@@ -75,6 +75,31 @@ bool is_the_box_safe(vector<vector<bool>> &board)
     return true;
 }
 
+void nqueens(vector<vector<bool>> &board, int current_queen, int last_queen_placed_in_which_box, string as_of_now)
+{
+    if (current_queen == board.size())
+    {
+
+        count++;
+        cout << as_of_now << endl;
+
+        // count++;
+        return;
+    }
+    for (int current_box = last_queen_placed_in_which_box + 1; current_box < board.size() * board.size(); current_box++)
+    {
+        int i = current_box / board[0].size();
+        int j = current_box % board[0].size();
+
+        if (board[i][j] == 0 && is_the_queen_safe(board, i, j) == true) //* this is where we have given a thought and have made a smart decision in order to make a Proactive Call
+        {
+            board[i][j] = 1;
+            nqueens(board, current_queen + 1, current_box, as_of_now + to_string(i) + to_string(j) + "_");
+            board[i][j] = 0;
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     vector<vector<bool>> board = {
