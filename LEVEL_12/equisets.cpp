0c18fdc199ea13d5ce27e24baef7a8c302c84bfa
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-13 22:07:03 +0530

    Update equisets.cpp

--- LEVEL_12/equisets.cpp
+++ LEVEL_12/equisets.cpp
@@ -18,6 +18,26 @@ void equisets(int arr[], int index, string set1, int sum_of_set1, string set2, i
     equisets(arr, index + 1, set1, sum_of_set1, set2 + arr[index] + " ", sum_of_set2 + arr[index], 7);
 }
 
+void equisetsv(int arr[], int index, vector<> set1, int sum_of_set1, vector<> set2, sum_of_set2, size_of_array) //* vector approach
+{
+    if (index == size_of_array)
+    {
+        if (sum_of_set1 == sum_of_set2)
+        {
+            cout << "set1" << set1 << " "
+                 << "set2" << set2;
+        }
+        return;
+    }
+
+    set1.push_back(arr[index]);
+    equisetsv(arr, index + 1, set1, sum_of_set1 + arr[index], set2, sum_of_set2, 7);
+    set1.pop_back(arr[index]);
+    set2.push_back(arr[index]);
+    equisets(arr, index + 1, set1, sum_of_set1, set2, sum_of_set2 + arr[index], 7);
+    set2.pop_back(arr[index]);
+}
+
 int main(int argc, char **argv)
 {
     int arr[] = {10, 20, 30, 40, 50, 60, 70};
