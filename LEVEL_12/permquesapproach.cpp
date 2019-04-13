cbb7816d7cb887a103ef8163999daafec2ef9d9f
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-13 22:10:31 +0530

    Update permquesapproach.cpp

--- LEVEL_12/permquesapproach.cpp
+++ LEVEL_12/permquesapproach.cpp
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+void permq(string ques, string ans)
+{
+    if (ques.size() == 0)
+    {
+        cout << ans << endl;
+        return;
+    }
+
+    for (int i = 0; i < ques.size(); i++)
+    {
+        char ch = ques[i];
+        string left = ques.substr(0, i);
+        string right = ques.substr(i + 1);
+        permq(left + right, ans + ch);
+    }
+}
+
 int main(int argc, char **argv)
 {
     permq("abc", "");
