4e9e1acda27394e6da8774c2ad52232b3cd5818b
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-13 22:05:56 +0530

    Update equisets.cpp

--- LEVEL_12/equisets.cpp
+++ LEVEL_12/equisets.cpp
@@ -2,6 +2,22 @@
 #include <string>
 #include <vector>
 
+void equisets(int arr[], int index, string set1, int sum_of_set1, string set2, int sum_of_set2, size_of_array)
+{
+    if (index == size_of_array)
+    {
+        if (sum_of_set1 == sum_of_set2)
+        {
+            cout << "set1" << set1 << " "
+                 << "set2" << set2;
+        }
+        return;
+    }
+
+    equisets(arr, index + 1, set1 + arr[index] + " ", sum_of_set1 + arr[index], set2, sum_of_set2, 7);
+    equisets(arr, index + 1, set1, sum_of_set1, set2 + arr[index] + " ", sum_of_set2 + arr[index], 7);
+}
+
 int main(int argc, char **argv)
 {
     int arr[] = {10, 20, 30, 40, 50, 60, 70};
