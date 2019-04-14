99cadad38520950e9b5eb8ce33b841aac8b81b3a
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-14 22:33:59 +0530

    Update permquestiondup.cpp

--- LEVEL_12/permquestiondup.cpp
+++ LEVEL_12/permquestiondup.cpp
@@ -3,6 +3,28 @@
 
 using namespace std;
 
+void permqd(string ques, string ans)
+{
+    if (ques.size() == 0)
+    {
+        cout << ans << endl;
+        return;
+    }
+
+    int bm = 0;
+    for (int i = 0; i < ques.size(); i++)
+    {
+        char ch = ques[i];
+        string left = ques.substr(0, i);
+        string right = ques.substr(i + 1);
+        if ((bm & (1 << (ch - 97))) == 0)
+        {
+            bm = bm | (1 << (ch - 97));
+            permqd(left + right, ans + ch);
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     permqd("baba", "");
