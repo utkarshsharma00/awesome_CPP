b71877aab4bd7fb5b646fe4914f4abb0cf8b68e4
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-16 22:09:54 +0530

    Update nqueensnprq.cpp

--- LEVEL_12/nqueensnprq.cpp
+++ LEVEL_12/nqueensnprq.cpp
@@ -56,6 +56,27 @@ bool is_the_queen_safe(vector<vector<bool>> &board, int i, int j)
     return true;
 }
 
+void nqueens(vector<vector<bool>> &board, int current_queen, string as_of_now)
+{
+    if (current_queen > board.size())
+    {
+        cout << as_of_now << endl;
+        return;
+    }
+    for (int current_box = 0; current_box < board.size() * board.size(); current_box++)
+    {
+        int i = current_box / board[0].size();
+        int j = current_box % board[0].size();
+
+        if (board[i][j] == 0 && is_the_queen_safe(board, i, j) == true)
+        {
+            board[i][j] = 1;
+            nqueens(board, current_queen + 1, as_of_now + to_string(i) + to_string(j) + "_");
+            board[i][j] = 0;
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     vector<vector<bool>> board{
