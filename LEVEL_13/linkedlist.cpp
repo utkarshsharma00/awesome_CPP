ca25dafad9c934f7a9f0c7084d6f827a6e741f45
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-18 20:33:44 +0530

    Update linkedlist.cpp

--- LEVEL_13/linkedlist.cpp
+++ LEVEL_13/linkedlist.cpp
@@ -1,3 +1,16 @@
 #include <iostream> //This has all the codes
 #include <bits/stdc++.h>
 using namespace std;
+
+class LinkedList
+{
+  private:
+    class Node
+    {
+      public:
+        int data;
+        Node *next;
+    };
+};  
+  
+  
