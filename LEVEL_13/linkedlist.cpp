a0792134b00b4d95be597afaeafb1a4b2bde051c
Utkarsh Sharma <flamingice2801@gmail.com>
2019-04-18 20:37:49 +0530

    Update linkedlist.cpp

--- LEVEL_13/linkedlist.cpp
+++ LEVEL_13/linkedlist.cpp
@@ -11,6 +11,22 @@ class LinkedList
         int data;
         Node *next;
     };
+  
+    Node *head;
+    Node *tail;
+    int size;
+  
+  public:
+    void display()
+    {
+        Node *temp = head;
+        for (int i = 0; i < size; i++)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
 };  
   
   
