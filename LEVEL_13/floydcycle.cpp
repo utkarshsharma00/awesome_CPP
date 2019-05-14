7ccf93131294da98ab730dd51a4c7f339713e544
Utkarsh Sharma <flamingice2801@gmail.com>
2019-05-14 21:38:42 +0530

    Update floydcycle.cpp

--- LEVEL_13/floydcycle.cpp
+++ LEVEL_13/floydcycle.cpp
@@ -35,3 +35,16 @@ void display(Node *n)
     }
     cout << endl;
 }
+
+int main(int argc, char **argv)
+{
+    Node *n1 = new Node(1);
+    Node *n2 = new Node(2);
+    Node *n3 = new Node(3);
+    Node *n4 = new Node(4);
+    Node *n5 = new Node(5);
+    Node *n6 = new Node(6);
+    Node *n7 = new Node(7);
+    Node *n8 = new Node(8);
+    Node *n9 = new Node(9);
+} 
