3066660b68c1bdd87b4f1efafb7249b3ae4c201c
Utkarsh Sharma <flamingice2801@gmail.com>
2019-05-17 00:25:05 +0530

    Update generictrees.cpp

--- LEVEL_14/generictrees.cpp
+++ LEVEL_14/generictrees.cpp
@@ -25,3 +25,11 @@ void display(Node *root)
 {
   //yet to be written 
 }
+
+int main(int argc, char **argv) //TODO calling the functions is a tricky task in this code
+{
+    vector<int> list{
+        10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+    Node *root = construct(list);
+
+}
