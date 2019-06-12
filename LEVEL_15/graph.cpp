2e549eb349acc8b121fb4b98cc82e784be09e2c5
Utkarsh Sharma <flamingice2801@gmail.com>
2019-06-12 22:35:29 +0530

    Create graph.cpp

---
+++ LEVEL_15/graph.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <climits>
+using namespace std;
+
+class Edge
+{
+public:
+    int neighbour;
+    int weight;
+
+    Edge(int neighbour, int weight)
+    {
+        this->neighbour = neighbour;
+        this->weight = weight;
+    }
+};
+
+void addEdge(vector<vector<Edge>> &graph, int vertex1, int vertex2, int weight)
+{
+    graph[vertex1].push_back(Edge(vertex2, weight));
+    graph[vertex2].push_back(Edge(vertex1, weight));
+}
+
+int main(int argc, char **argv)
+{
+}
