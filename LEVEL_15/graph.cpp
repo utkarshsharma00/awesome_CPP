9e90ae9baa76aee73dd3f3430420450457aa873e
Utkarsh Sharma <flamingice2801@gmail.com>
2019-06-12 22:35:47 +0530

    Update graph.cpp

--- LEVEL_15/graph.cpp
+++ LEVEL_15/graph.cpp
@@ -26,4 +26,16 @@ void addEdge(vector<vector<Edge>> &graph, int vertex1, int vertex2, int weight)
 
 int main(int argc, char **argv)
 {
+    vector<vector<Edge>> graph(9);
+    addEdge(graph, 0, 1, 10);
+    addEdge(graph, 1, 2, 10);
+    addEdge(graph, 2, 3, 10);
+    addEdge(graph, 0, 3, 40);
+    addEdge(graph, 3, 4, 2);
+    addEdge(graph, 4, 5, 3);
+    addEdge(graph, 4, 6, 8);
+    addEdge(graph, 5, 6, 3);
+    addEdge(graph, 7, 0, 2);
+    addEdge(graph, 7, 8, 2);
+    addEdge(graph, 8, 0, 2);
 }
