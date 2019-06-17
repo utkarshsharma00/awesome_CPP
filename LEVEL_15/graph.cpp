dce1be375093f314c69c920f11e56346718736a5
Utkarsh Sharma <flamingice2801@gmail.com>
2019-06-17 21:58:54 +0530

    Update graph.cpp

--- LEVEL_15/graph.cpp
+++ LEVEL_15/graph.cpp
@@ -89,6 +89,19 @@ void min_Path(vector<vector<Edge>> &graph, int source, int destination, vector<b
     visited[source] = false;
 }
 
+void max_Path(vector<vector<Edge>> &graph, int source, int destination, vector<bool> &visited, string answer_so_far, int sum)
+{
+    if (source == destination)
+    {
+        if (Max < sum)
+        {
+            Max = sum;
+            maxpath = answer_so_far + to_string(destination);
+        }
+        return;
+    }
+}
+
 int main(int argc, char **argv)
 {
     vector<vector<Edge>> graph(9);
