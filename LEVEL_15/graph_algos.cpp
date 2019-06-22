f8f483d297f5ad8947826fa14eed653d60940442
Utkarsh Sharma <flamingice2801@gmail.com>
2019-06-22 20:22:13 +0530

    Create graph_algos.cpp

---
+++ LEVEL_15/graph_algos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <queue>
+#include <string>
+#include <climits>
+
+using namespace std;
+
+class Edge
+{
+public:
+    int neighbour;
+    int weight;
+
+    Edge(int neighbour, int weight)
+    {
+        this->neighbour = neighbour;
+        this->weight = weight;
+    }
+};
