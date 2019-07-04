d14f924ccfe161e130367c63b7f3850715e6f48d
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-04 16:42:26 +0100

    Update largest_increasing_subseq.cpp

--- LEVEL_16/largest_increasing_subseq.cpp
+++ LEVEL_16/largest_increasing_subseq.cpp
@@ -4,6 +4,23 @@
 #include <climits>
 using namespace std;
 
+void lis(vector<int> &arr)
+{
+    vector<int> strg(arr.size(), 1);
+
+    //strg[0] = 0;
+    for (int i = 1; i < strg.size(); i++)
+    {
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] < arr[i])
+            {
+                strg[i] = max(strg[i], strg[j] + 1);
+            }
+        }
+    }
+}
+
 int main(int argc, char** argv)
 {
     vector<int> arr{10, 22, 9, 33, 21, 50, 41, 60, 80, 1};
