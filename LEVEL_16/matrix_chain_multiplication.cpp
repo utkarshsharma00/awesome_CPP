c932b15f0ac8f18c96fc72fc7f72f6dc1895d053
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-06 22:06:54 +0100

    Create matrix_chain_multiplication.cpp

---
+++ LEVEL_16/matrix_chain_multiplication.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void matrix_chain_multiplication(vector<int> &matrix_arr)
+{
+    
+}
+
+int main(int argc, char **argv)
+{
+    vector<int> matrix_arr{10, 20, 30, 40, 50, 60};
+    matrix_chain_multiplication(matrix_arr);
+}
