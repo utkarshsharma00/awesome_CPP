4ca54e867e965f8f322e7749142319b116f6a09c
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-09 20:29:33 +0100

    Update tarsum.cpp

--- LEVEL_16/tarsum.cpp
+++ LEVEL_16/tarsum.cpp
@@ -7,6 +7,18 @@ using namespace std;
 void tarsum(vector<int> &arr, int target)
 {
     vector<vector<bool>> storage(arr.size() + 1, vector<bool>(target + 1, false));
+    for (int i = 0; i < storage.size(); i++)
+    {
+        for (int j = 0; j < storage[0].size(); j++)
+        {
+            if (i == 0 && j == 0) //To Put All true in the first col
+            {
+                storage[i][j] = true;
+            }
+            else if (i == 0) //To Put All false in the first row
+            {
+                storage[i][j] = false;
+            }
 }
 
 int main(int argc, char **argv)
