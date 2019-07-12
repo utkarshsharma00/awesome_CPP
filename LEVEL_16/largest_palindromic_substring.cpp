432566d6809340d815bbe548b0cb47d144df8403
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-12 18:37:34 +0100

    Update largest_palindromic_substring.cpp

--- LEVEL_16/largest_palindromic_substring.cpp
+++ LEVEL_16/largest_palindromic_substring.cpp
@@ -4,6 +4,21 @@
 
 using namespace std;
 
+int length = 0;
+void largest_palin_substring(string s)
+{
+    vector<vector<bool>> storage(s.length(), vector<bool>(s.length()));
+
+    for (int gap = 0; gap < s.size(); gap++)
+    {
+        for (int i = 0, j = i + gap; j < storage.size(); i++, j++)
+        {
+            if (gap == 0)
+            {
+                storage[i][j] = 1;
+            }
+}
+
 int main()
 {
     largest_palin_substring("abccbc");
