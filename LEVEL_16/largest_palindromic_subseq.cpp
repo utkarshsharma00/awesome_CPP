48d292069c898b7ebb3bb377541151b580f8b788
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-12 18:34:27 +0100

    Update largest_palindromic_subseq.cpp

--- LEVEL_16/largest_palindromic_subseq.cpp
+++ LEVEL_16/largest_palindromic_subseq.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+void LPS(string s1)
+{
+    vector<vector<int>> storage(s1.length(), vector<int>(s1.length()));
+
+    for (int gap = 0; gap < s1.size(); gap++)
+    {
+        for (int i = 0, j = i + gap; j < storage.size(); i++, j++)
+        {
+            if (gap == 0)
+            {
+                storage[i][j] = 1;
+            }
+}
+
 int main()
 {
     LPS("abkccgbc");
