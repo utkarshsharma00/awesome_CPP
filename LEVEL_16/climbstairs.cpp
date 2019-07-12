71b63b100e84152d1b10532c704682126a02ffa7
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-12 18:35:57 +0100

    Update climbstairs.cpp

--- LEVEL_16/climbstairs.cpp
+++ LEVEL_16/climbstairs.cpp
@@ -2,13 +2,25 @@
 #include <vector>
 using namespace std;
 
+void printstairs(int no_of_stairs, string as_of_now)
+{
+    static int counter = 0;
+    if (no_of_stairs == 0)
+    {
+        cout << as_of_now << endl;
+        counter++;
+        cout << counter;
+        return;
+    }
+}
+
 int main(int argc, char **argv)
 {
-    //printstairs(7, "");
-    // int n = 7;
-    // vector<int> ques(n + 1, 0);
-    // cout << printstairsMemoisation(7, ques);
+    printstairs(7, "");
+    int n = 7;
+    vector<int> ques(n + 1, 0);
+    cout << printstairsMemoisation(7, ques);
 
-    vector<int> jumpSize{0, 2, 3, 1, 0, 2, 0, 2, 4};
-    cout << climbstairs2(jumpSize) << endl;
+    //vector<int> jumpSize{0, 2, 3, 1, 0, 2, 0, 2, 4};
+    //cout << climbstairs2(jumpSize) << endl;
 }
