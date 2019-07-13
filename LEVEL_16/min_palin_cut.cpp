0e27f7e64b279ee274d963d935271df3346df283
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-13 21:23:38 +0100

    Create min_palin_cut.cpp

---
+++ LEVEL_16/min_palin_cut.cpp
@@ -0,0 +1,17 @@
+#include <iostream> // Tough question in DP, will definitely be asked somewhere
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void minimum_palin_cut(string s)
+{
+    vector<vector<int>> storage(s.length(), vector<int>(s.length()));
+    
+}
+
+int main(int argc, char **argv)
+{
+    minimum_palin_cut("abccbc");
+}
