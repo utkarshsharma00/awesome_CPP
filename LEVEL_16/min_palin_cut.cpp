c751a68996df92626901382d1f084b8f2b860bc2
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-14 20:14:48 +0100

    Update min_palin_cut.cpp

--- LEVEL_16/min_palin_cut.cpp
+++ LEVEL_16/min_palin_cut.cpp
@@ -27,6 +27,27 @@ void minimum_palin_cut(string s)
                     storage[i][j] = 1;
                 }
             }
+            else
+            {
+                if (s[i] == s[j] && storage[i + 1][j - 1] == 0) //s[i] and s[j] indicate the start and end points
+                {
+                    storage[i][j] = 0;
+                }
+                else
+                {
+                    int mymin = s.size();
+                    for (int k = 0; k < gap; k++)
+                    {
+                        int lhs = storage[i][j - gap + k]; //this is basically the start point
+                        int rhs = storage[i + 1 + k][j];
+                        mymin = min(lhs + rhs, mymin);
+                    }
+
+                    storage[i][j] = mymin + 1;
+                }
+            }
+        }
+    }   
     
 }
 
