1d61cae25824067fd94348843a3b1db3068a9d76
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-15 16:34:53 +0100

    Update stepsknown.cpp

--- LEVEL_16/stepsknown.cpp
+++ LEVEL_16/stepsknown.cpp
@@ -2,6 +2,12 @@
 #include <vector>
 using namespace std;
 
+int stepsknown(vector<int> &jumpSize) //new technique which is known as Tabulation
+{
+    vector<int> strg(jumpSize.size(), 0); //strg means storage
+
+}
+
 int main()
 {
     vector<int> jumpSize{0, 2, 3, 1, 0, 2, 0, 2, 4};
