1b9795cad424cc17fd219c62dc2583048c3a1ef8
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-17 15:39:08 +0100

    Create countaibjck.cpp

---
+++ LEVEL_16/countaibjck.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void countaibjck(string s)
+{
+    int counta = 0;
+    int countb = 0;
+    int countc = 0;
+}
+
+int main()
+{
+    countaibjck("abcabc");
+}
