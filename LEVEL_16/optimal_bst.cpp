4b74b29f8c74182908cc03e5ee97be18a81490b1
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-17 15:34:10 +0100

    Create optimal_bst.cpp

---
+++ LEVEL_16/optimal_bst.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //see this code for errors
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    vector<int> elements = {5, 7, 8, 17, 20};
+    vector<int> frequency = {4, 7, 5, 1, 2};
+
+    optimal_bst(elements, frequency);
+}
