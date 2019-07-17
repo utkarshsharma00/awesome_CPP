84ac0b9f6f16209087a51cd372cc80d1b147a109
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-17 15:33:02 +0100

    Update minsteps.cpp

--- LEVEL_16/minsteps.cpp
+++ LEVEL_16/minsteps.cpp
@@ -4,6 +4,12 @@
 #include <climits>
 using namespace std;
 
+int countMinSteps(vector<int> &jumpSize)
+{
+    vector<int> strg(jumpSize.size(), 0);
+    strg[0] = 0;
+}
+
 int main()
 {
     vector<int> jumpSize{0, 2, 3, 2, 0, 2, 0, 2, 4};
