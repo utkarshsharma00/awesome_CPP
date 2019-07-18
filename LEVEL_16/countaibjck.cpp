23a7c81b52e6d59d16a6b3420866fc4c66fcfeeb
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-18 21:27:09 +0100

    Update countaibjck.cpp

--- LEVEL_16/countaibjck.cpp
+++ LEVEL_16/countaibjck.cpp
@@ -10,6 +10,24 @@ void countaibjck(string s)
     int counta = 0;
     int countb = 0;
     int countc = 0;
+    
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'a')
+        {
+            counta = 2 * counta + 1;
+        }
+        else if (s[i] == 'b')
+        {
+            countb = 2 * countb + counta;
+        }
+        else
+        {
+            countc = 2 * countc + countb;
+        }
+    }
+    
+    cout << countc << endl;
 }
 
 int main()
