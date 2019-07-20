3ebdca318413924b9b8c39341992646d30363254
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-20 20:06:50 +0100

    Create count_palin_subseq.cpp

---
+++ LEVEL_16/count_palin_subseq.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //* In this code we find the answer of the count at the topmost right corner of the printed storage vector
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int counter = 0;
+void count_palindromic_subseq(string s)
+{
+    vector<vector<int>> storage(s.length(), vector<int>(s.length()));
+}
