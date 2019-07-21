e3b54dfe90cd9ba2c17a8fbeac4fc01e56fec9ec
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-21 21:34:04 +0100

    Update build_your_own_pq.cpp

--- LEVEL_17/build_your_own_pq.cpp
+++ LEVEL_17/build_your_own_pq.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 class Priority_queue
 {
+private:
+    vector<int> list;
+    bool type; //true for creating a MinHeap and false for creating a MaxHeap
 
+    bool is_higher_priority(int i, int j)
+    {
+        if (this->type == true)
+        {
+            return list[i] < list[j]; //will create a MinHeap
+        }
+
+        else
+        {
+            return list[i] > list[j]; //will create a MaxHeap
+        }
+    }
 };
 
 int main(int argc, char **argv)
