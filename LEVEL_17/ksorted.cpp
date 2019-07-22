7f05d8650b492cb9f41a7f41b3c0292375a93d90
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-22 20:54:51 +0100

    Create ksorted.cpp

---
+++ LEVEL_17/ksorted.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void ksorted(vector<int> &arr, int k)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+    vector<int> arr{20, 40, 10, 30, 50, 80, 60, 90, 70, 120, 110, 100};
+    ksorted(arr, 2);
+}
