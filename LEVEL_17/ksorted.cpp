0c36ab1cacc9b105608c7d41b6c4d67020f78c83
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-23 21:28:10 +0100

    Update ksorted.cpp

--- LEVEL_17/ksorted.cpp
+++ LEVEL_17/ksorted.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 void ksorted(vector<int> &arr, int k)
 {
-
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i = 0; i <= k; i++)
+    {
+        pq.push(arr[i]);
+    }
 }
 
 int main(int argc, char **argv)
