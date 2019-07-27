95650f917542300d70449401a4e90e0b74dd6c61
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-27 18:41:06 +0100

    Update stackandqueue.cpp

--- LEVEL_18/stackandqueue.cpp
+++ LEVEL_18/stackandqueue.cpp
@@ -14,4 +14,16 @@ class Interval
 public:
     int start;
     int end;
+    
+    Interval(int start, int end)
+    {
+        this->start = start;
+        this->end = end;
+    }
+    
+    //* Operator Overloading!
+    bool operator<(const Interval &O) const //*this const is for 'this'
+    {
+        return this->start < O.start;
+    }
 };
