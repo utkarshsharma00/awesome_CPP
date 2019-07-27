6bf316f27abf08e01fa123d9ab02a7de84a93ddf
Utkarsh Sharma <flamingice2801@gmail.com>
2019-07-27 18:39:02 +0100

    Update kstacks.cpp

--- LEVEL_18/kstacks.cpp
+++ LEVEL_18/kstacks.cpp
@@ -15,4 +15,16 @@ private:
 int main(int argc, char **argv)
 {
     kstacks ks(10, 3);
+    
+    ks.push(10, 0);
+    ks.push(20, 0);
+    ks.push(30, 1);
+    ks.push(40, 2);
+    ks.push(50, 2);
+    ks.push(60, 1);
+    ks.push(70, 1);
+    ks.push(80, 2);
+    ks.push(90, 0);
+    ks.push(100, 0);
+    ks.push(110, 2);
 }
