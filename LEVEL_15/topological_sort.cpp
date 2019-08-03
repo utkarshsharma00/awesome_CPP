101c88661c3ba042ed44aeca8df6259e195a9770
Utkarsh Sharma <flamingice2801@gmail.com>
2019-08-03 20:57:01 +0100

    Update topological_sort.cpp

--- LEVEL_15/topological_sort.cpp
+++ LEVEL_15/topological_sort.cpp
@@ -27,4 +27,17 @@ void addEdge(vector<vector<Edge>> &graph, int vertex1, int vertex2, int weight)
     graph[vertex2].push_back(Edge(vertex1, weight));
 }
 
+int main(int argc, char **argv)
+{
+    vector<vector<Edge>> graph(7);
+    graph[0].push_back(Edge(1, 1));
+    graph[0].push_back(Edge(4, 1));
+    graph[1].push_back(Edge(2, 1));
+    graph[2].push_back(Edge(3, 1));
+    graph[6].push_back(Edge(4, 1));
+    graph[6].push_back(Edge(5, 1));
+    graph[5].push_back(Edge(3, 1));
+    topological_sort(graph);
+}
+
 
