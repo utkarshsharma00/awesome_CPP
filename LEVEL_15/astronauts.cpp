25ed262479651446cd2640e95011ae481d62f818
Utkarsh Sharma <flamingice2801@gmail.com>
2019-08-05 20:52:37 +0100

    Update astronauts.cpp

--- LEVEL_15/astronauts.cpp
+++ LEVEL_15/astronauts.cpp
@@ -23,6 +23,33 @@ void addEdge(vector<vector<Edge>> &graph, int v1, int v2, int weight)
     graph[v2].push_back(Edge(v1, weight));
 }
 
+int getsize(vector<vector<Edge>> &graph, vector<bool> &visited, int src)
+{
+    
+}
+
+int astro(vector<int> &d1, vector<int> &d2, int vertices)
+{
+    
+}
+
+void display(vector<vector<Edge>> &graph)
+{
+    cout << "---------------------------" << endl;
+    for (int v = 0; v < graph.size(); v++)
+    {
+        cout << v << "->"
+             << " ";
+        for (int n = 0; n < graph[v].size(); n++)
+        {
+            Edge ed = graph[v][n];
+            cout << "[" << ed.n << "--" << ed.w << "]";
+        }
+        cout << endl;
+    }
+    cout << "---------------------------" << endl;
+}
+
 int main(int argc, char **argv)
 {
     vector<int> d1 = {1, 6, 8, 2, 9, 10, 0};
