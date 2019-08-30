70df884e8f623e4bc915db42806f267c3bb458fa
Utkarsh Sharma <flamingice2801@gmail.com>
2019-08-30 17:39:41 +0100

    Update print_highest_freq_char.cpp

--- LEVEL_19/print_highest_freq_char.cpp
+++ LEVEL_19/print_highest_freq_char.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <unordered_map>
 #include <bits/stdc++.h>
+using namespace std:
 
+void highest_freq(string s)
+{
+    unordered_map<char, int> freq_map;
+    
+    //yet to be written
+}
 int main(int argc, char **argv)
 {
     highest_freq("abbccddaccbdacdcba");
