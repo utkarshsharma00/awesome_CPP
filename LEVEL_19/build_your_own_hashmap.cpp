fe0a0e587950df386f0994e844e60715f6a810f4
Utkarsh Sharma <flamingice2801@gmail.com>
2019-08-31 20:10:27 +0100

    Update build_your_own_hashmap.cpp

--- LEVEL_19/build_your_own_hashmap.cpp
+++ LEVEL_19/build_your_own_hashmap.cpp
@@ -5,8 +5,27 @@
 using namespace std;
 
 template <class K, class V>
+    
+class HashMap
+{
+    private:
+    int no_of_elements;
+    int no_of_buckets;
 
-//yet to be written
+    class HashMap_Node
+    {
+    public:
+        K key;
+        V value;
+        HashMap_Node(K key, V value)
+        {
+            this->key = key;
+            this->value = value;
+        }
+    };
+    
+    //yet to be written
+};
 
 int main(int argc, char **argv)
 {
