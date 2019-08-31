cf6f886c76a2ac4ce41d1312bf83f613b7671efa
Utkarsh Sharma <flamingice2801@gmail.com>
2019-08-31 20:06:43 +0100

    Create longest_subseq_of_consecutive_elements.cpp

---
+++ LEVEL_19/longest_subseq_of_consecutive_elements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void longest_subseq_of_consecutive_elements(vector<int> &arr)
+{
+    unordered_map<int, bool> map;
+    
+    //yet to be written
+}
+
+int main(int argc, char **argv)
+{
+    vector<int> arr{11, 3, 5, 9, 12, 2, 6, 8, 1, 10, 15, 25, 20, 16};
+    longest_subseq_of_consecutive_elements(arr);
+}
