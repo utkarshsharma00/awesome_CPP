0e6bdd0175aead8b7a48cc2b14d18d750f366db6
Utkarsh Sharma <flamingice2801@gmail.com>
2019-09-11 19:29:29 +0530

    Update binary_search_tree.cpp

--- LEVEL_14/binary_search_tree.cpp
+++ LEVEL_14/binary_search_tree.cpp
@@ -5,6 +5,21 @@
 
 using namespace std;
 
+class Node
+{
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
 int main(int argc, char **argv)
 {
   //yet to be written
