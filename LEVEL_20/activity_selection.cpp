28207faf1e2f6fbe405ab1235806b14fd1b0e074
Utkarsh Sharma <flamingice2801@gmail.com>
2019-09-25 20:48:04 +0530

    Create activity_selection.cpp

---
+++ LEVEL_20/activity_selection.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    priority_queue<Activity, vector<Activity>, greater<Activity>> pq;
+
+    vector<int> start{3, 0, 1, 8, 5, 5};
+    vector<int> end{4, 6, 2, 9, 9, 7};
+}
