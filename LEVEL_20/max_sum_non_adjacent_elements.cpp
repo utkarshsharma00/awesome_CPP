fa3d42032cd0d13df96910e53fe00f27deb2572e
Utkarsh Sharma <flamingice2801@gmail.com>
2019-09-28 07:37:49 +0530

    Create max_sum_non_adjacent_elements.cpp

---
+++ LEVEL_20/max_sum_non_adjacent_elements.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    vector<int> arr{5, 6, 10, 100, 10};
+    max_sum_non_adjacent_elements(arr);
+}
