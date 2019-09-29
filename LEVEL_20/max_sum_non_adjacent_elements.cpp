e34b6f8b317feba8831b5c442ef7131f2ebd35eb
Utkarsh Sharma <flamingice2801@gmail.com>
2019-09-29 20:03:06 +0530

    Update max_sum_non_adjacent_elements.cpp

--- LEVEL_20/max_sum_non_adjacent_elements.cpp
+++ LEVEL_20/max_sum_non_adjacent_elements.cpp
@@ -5,6 +5,23 @@
 
 using namespace std;
 
+void max_sum_non_adjacent_elements(vector<int> &arr)
+{
+    int inclusive = arr[0];
+    int exclusive = 0;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        int temp_inclusive = inclusive;
+        int temp_exclusive = exclusive;
+
+        inclusive = temp_exclusive + arr[i];
+        exclusive = max(temp_inclusive, temp_exclusive);
+    }
+
+    cout << inclusive << " " << exclusive << endl;
+}
+
 int main(int argc, char **argv)
 {
     vector<int> arr{5, 6, 10, 100, 10};
